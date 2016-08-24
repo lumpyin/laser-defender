cf7a24b69dafacaec1f48777078e28dbdf87eed7
yizhi <zyzisgood@gmail.com>
2016-08-24 00:26:45 -0500

    first commit

---
+++ Assets/Entities/Player/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+
+    public float speed = 15.0f;
+    public float padding = 1f;
+    public GameObject projectile;
+    public float projectileSpeed;
+
+    float xmin;
+    float xmax;
+
+	// Use this for initialization
+	void Start () {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+        xmin = leftmost.x + padding;
+        xmax = rightmost.x - padding;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+           GameObject beam = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+            beam.rigidbody2D.velocity = new Vector3(0,projectileSpeed,0);
+        }
+
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+        transform.position = new Vector3(newX,transform.position.y,transform.position.z);
+
+	}
+}
